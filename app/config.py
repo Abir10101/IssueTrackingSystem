e7d6403155d50fed99f1d9d8f124da04031b76ed
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2023-07-17 07:36:29 +0530

    docker

--- app/config.py
+++ app/config.py
@@ -7,6 +7,8 @@ class Config:
     DB_USER = os.getenv('DB_USER', default = 'root')
     DB_PASSWORD = os.getenv('DB_PASSWORD', default = 'mysql')
     DB_NAME = os.getenv('DB_NAME', default = 'myapp')
+    DB_HOST = os.getenv('DB_HOST', default = 'localhost')
+    DB_PORT = os.getenv('DB_PORT', default = '3306')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Other configuration
     SECRET_KEY = os.getenv('SECRET_KEY', default = "terrible secret key")
@@ -15,7 +17,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_HOST')
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
 
 if os.getenv('FLASK_ENV') == 'prod':
     app_config = ProductionConfig()
